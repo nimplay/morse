13a45b502e461697fdbe97c061743b41a82df084
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-09-20 23:19:00 +0500

    Decode functions update
    Added following functions:
    - decode_word
    - decode
    also added tests

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -49,3 +49,31 @@ def split_sentence(sentence)
     puts 'Your input should be a string in double or single quote!'
   end
 end
+
+def decode_word(word)
+  if word.is_a? String
+    word_decode = ''
+    word.split.each do |morse|
+      dc = decode_char(morse)
+      word_decode += dc.to_s
+    end
+  end
+  word_decode
+end
+
+def decode(sentence)
+  words = split_sentence(sentence)
+  sentence_decode = ''
+  words.each do |word|
+    word_decode = decode_word(word)
+    sentence_decode += "#{word_decode} "
+  end
+  sentence_decode.strip
+end
+
+# Test
+p decode_char('-...') #=> B
+p decode_word('-- -.--') # => MY
+p decode('-- -.--   -. .- -- .') # => MY NAME
+sentence = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+p decode(sentence) # => A BOX FULL OF RUBIES
